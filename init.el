a071f532b6c2c03e6237fe43f522bf85da6d03b3
KazukiKatayama <kkktym@KazukiKatayamanoMacBook-Air.local>
2020-03-04 16:06:05 +0900

    new

---
+++ init.el
@@ -0,0 +1,28 @@
+;; key bind
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "C-c C-h") 'help-command)
+
+;; scroll
+(setq scroll-step 1)
+
+(require 'package)
+
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; MELPA-stableを追加
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+;; Marmaladeを追加
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+;; Orgを追加
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+;; 初期化
+(package-initialize)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
